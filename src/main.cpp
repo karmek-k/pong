f5ec9cd0bf6047c07419fbfb632bbe6e7083fe89
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-03-26 15:20:44 +0100

    Window size is now changeable

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+
 #include "Game.h"
 
-int main() {
-    Game(800u, 600u);
+constexpr unsigned DEFAULT_WIDTH = 800u, DEFAULT_HEIGHT = 600u;
+
+int main(int args, char* argv[]) {
+    unsigned width = DEFAULT_WIDTH, height = DEFAULT_HEIGHT;
+
+    // read window size from argv
+    if (args >= 3) {
+        try {
+            width = std::stoi(argv[1]);
+            height = std::stoi(argv[2]);
+        }
+        catch (std::invalid_argument& e) {
+            std::cerr << "Invalid arguments specified!\n";
+            std::exit(1);
+        }
+    }
+
+    Game(width, height);
+
     return 0;
 }
